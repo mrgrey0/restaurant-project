ceba93a2b5305428bcf54210fce93953c6b9b7bc
mrgrey0 <108786019+mrgrey0@users.noreply.github.com>
2024-10-02 14:41:56 +0530

    dynamic designs added
    dynamic product page added

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import path
-from main.views import menu
+from main.views import category, menu
 
 urlpatterns = [
-    path('menu/',menu, name='menu')
+    path('menu/',menu, name='menu'),
+    path('category/<str:category>/', category, name='category'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
 
+def category(request, category):
+    # Fetch food items for the given category from the database or any data source
+    # For simplicity, let's use a sample dictionary for now
+    food_categories = {
+    'breakfast': ['Pancakes', 'Omelette', 'French Toast', 'Waffles'],
+    'veg-food': ['Vegetable Curry', 'Salad', 'Paneer Butter Masala', 'Mushroom Soup'],
+    'non-veg-food': ['Chicken Wings', 'Fish Curry', 'Mutton Biryani', 'Chicken Tikka'],
+    'fast-food': ['Burgers', 'Fries', 'Pizza', 'Sandwich'],
+    'cold-drink': ['Coke', 'Pepsi', 'Lemonade', 'Iced Tea'],
+    'desserts': ['Ice Cream', 'Cake', 'Brownies', 'Cupcakes']
+    }
+
+    items = food_categories.get(category, [])
+    return render(request, 'category_page.html',{'category':category, 'food_items':items})
 
 def menu(request):
-    
     return render(request, 'menucard.html')
